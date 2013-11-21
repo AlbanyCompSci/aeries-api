b7770288bb5331711c01686431380fc6cf2bfdb7
David Harrison <djrh777@gmail.com>
2013-11-20 16:13:25 -0800

    migrate to Selenium (allows use of Javascript)

--- AeriesSession.py
+++ AeriesSession.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python2
 
-import mechanize
+from selenium import webdriver
 
 BASEADDRESS = 	'https://abi.ausdk12.org/aeriesportal/'
 LOGIN_PAGE = 	'LoginParent.aspx'
-FORM_ID =	'form1'
+#FORM_ID =	'form1'
 EMAIL_ID =	'txtEmailAddress'
 PASSWORD_ID =	'txtPassword'
+LOGIN_ID =      'btnLogin'
 
 class Session:
 
-	global br
+	global driver
 
 	def __init__(self, email, password):
-		self.br = mechanize.Browser()
-		self.br.open(BASEADDRESS + LOGIN_PAGE)
-		self.br.select_form(FORM_ID)
-		self.br[EMAIL_ID] = email
-		self.br[PASSWORD_ID] = password
-		self.br.submit()
+                self.driver = webdriver.PhantomJS()
+                self.driver.get(BASEADDRESS + LOGIN_PAGE)
+                email_elem = self.driver.find_element_by_id(EMAIL_ID)
+                email_elem.send_keys(email)
+                password_elem = self.driver.find_element_by_id(PASSWORD_ID)
+                password_elem.send_keys(password)
+                login_elem = self.driver.find_element_by_id(LOGIN_ID)
+                login_elem.click()
 
 	def getPage(self, address):
-		response = self.br.open(address)
-		return response.read()
+		self.driver.get(address)
+		return self.driver.page_source
